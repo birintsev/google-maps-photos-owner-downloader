9d0a0f0611b0db6a6e8a40ea3b12e71b4d70b939
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2023-07-14 12:10:01 +0300

    @CompletedDownload was added

---
+++ src/main/java/birintsev/google/maps/photos/owner/annotations/CompletedDownload.java
@@ -0,0 +1,14 @@
+package birintsev.google.maps.photos.owner.annotations;
+
+import birintsev.google.maps.photos.owner.validators.CompletedDownloadValidator;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = CompletedDownloadValidator.class)
+public @interface CompletedDownload {
+
+}
