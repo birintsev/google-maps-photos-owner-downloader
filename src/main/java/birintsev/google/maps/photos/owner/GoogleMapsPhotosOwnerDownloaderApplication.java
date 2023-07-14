0c10dc87afbfdbc14c4536a0f0b3791c306898c1
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2023-07-14 12:07:37 +0300

    GoogleMapsPhotosOwnerDownloaderApplication was added

---
+++ src/main/java/birintsev/google/maps/photos/owner/GoogleMapsPhotosOwnerDownloaderApplication.java
@@ -0,0 +1,12 @@
+package birintsev.google.maps.photos.owner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapsPhotosOwnerDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleMapsPhotosOwnerDownloaderApplication.class, args);
+    }
+}
