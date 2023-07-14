380ee95d5da5448d714cb4e661c120e3e79f134f
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2023-07-14 12:08:16 +0300

    Constants was added

---
+++ src/main/java/birintsev/google/maps/photos/owner/constants/Constants.java
@@ -0,0 +1,10 @@
+package birintsev.google.maps.photos.owner.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+    public static final String HTML_TAG_LINK = "a";
+}
