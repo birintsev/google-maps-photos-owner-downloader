e3b767ec7a49af61e7b6fc65a4210750b0b8f764
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2023-07-14 16:05:32 +0300

    GooglePhotoDownloadSaver.save(): arguments were annotated with @Valid and @CompletedDownload

--- src/main/java/birintsev/google/maps/photos/owner/services/GooglePhotoDownloadSaver.java
+++ src/main/java/birintsev/google/maps/photos/owner/services/GooglePhotoDownloadSaver.java
@@ -1,10 +1,12 @@
 package birintsev.google.maps.photos.owner.services;
 
+import birintsev.google.maps.photos.owner.annotations.CompletedDownload;
 import birintsev.google.maps.photos.owner.dto.GooglePhotoDownload;
+import jakarta.validation.Valid;
 
 import java.nio.file.Path;
 
 public interface GooglePhotoDownloadSaver {
 
-    Path save(GooglePhotoDownload googlePhotoDownload);
+    Path save(@Valid  @CompletedDownload GooglePhotoDownload googlePhotoDownload);
 }
